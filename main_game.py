4ba6166a8e80712e2b1ca01486ab9d92421ddbc6
Gary <131410326+ggraci11@users.noreply.github.com>
2023-09-12 09:03:18 -0500

    Surface and Grid
    Making the surface, grid, and colors

--- main_game.py
+++ main_game.py
@@ -1,8 +1,31 @@
+def drawGrid (w,rows,surface):
+    
+    pass
+
+
+def redrawWindow (surface):
+    global rows, width
+    win.fill((0,0,0))
+    drawGrid(surface)
+    pygame.display.update
+
+
+
 def main():
+    global width, rows
     width = 500
-    height = 500
     rows = 20
     win = pygame.display.set_mode((width, height))
+    s = snake((225,0,0), (10,10))
+    flag = True
+
+    clock = pygame.time.Clock() 
+
+    while flag:
+        pygame.time.delay(50)
+        clock.tick(10)
+        redrawWindow(win)
+
     pass
 
 
