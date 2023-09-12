13a199509c9cf379d26d48ac66d09d91af5523ce
Gary <131410326+ggraci11@users.noreply.github.com>
2023-09-12 09:12:23 -0500

    Update main_game.py

--- main_game.py
+++ main_game.py
@@ -1,12 +1,30 @@
+import math
+import random
+import pygame
+import tkinter as tk
+from tkinter import messagebox
+
+
+
 def drawGrid (w,rows,surface):
-    
+    sizeBtwn = w // rows
+
+    x= 0
+    y=0
+    for l in range (rows):
+        x = x + sizeBtwn
+        y = y + sizeBtwn
+
+        pygame.draw.line(surface, (255,255,255), (x,0), (x,w))
+        pygame.draw.line(surface, (255,255,255), (0,y), (w,y))
+
     pass
 
 
 def redrawWindow (surface):
     global rows, width
-    win.fill((0,0,0))
-    drawGrid(surface)
+    surface.fill((0,0,0))
+    drawGrid(width, rows, surface)
     pygame.display.update
 
 
@@ -15,7 +33,7 @@ def main():
     global width, rows
     width = 500
     rows = 20
-    win = pygame.display.set_mode((width, height))
+    win = pygame.display.set_mode((width, width))
     s = snake((225,0,0), (10,10))
     flag = True
 
